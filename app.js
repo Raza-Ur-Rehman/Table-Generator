126360034a5ba9a74fac2805be66381a54fc68f2
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-07 01:06:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-let number = document.getElementById('table');
-let range = document.getElementById('range');
-let tableBox = document.getElementById('tablebox');
 
 function tableGenerate() {
-    for (let i = 0; i < array.length; i++) {
-        
-        
+    let input = document.getElementById('table');
+    let range = document.getElementById('range');
+    let tableBox = document.getElementById('tablebox');
+    let show = document.getElementById('showtable');
+    if (range <= '') {
+        Swal.fire({
+            icon: "error",
+            title: "Invaild",
+            text: "Please Enter a Number",
+        });
+    } else {
+        for (let i = 1; i <= range.value; i++) {
+            let result = input * i;
+            show.innerText = `${input} * ${i} = ${result} `;
+            
+
+
+            console.log(result);
+        }
     }
 }
 
