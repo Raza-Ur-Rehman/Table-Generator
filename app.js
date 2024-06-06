8d3724559c42952a47041eb6766d17ff7c8f214e
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-07 00:13:22 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-let
+let number = document.getElementById('table');
+let range = document.getElementById('range');
+let tableBox = document.getElementById('tablebox');
+
+function tableGenerate() {
+    for (let i = 0; i < array.length; i++) {
+        
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
