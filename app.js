e0994d96ff8c264ee2e92f8aa72feeee207ab790
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-21 01:10:42 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 
 function tableGenerate() {
-    let input = document.getElementById('table').value;
-    let range = document.getElementById('range').value;
+    let input = document.getElementById('table');
+    let range = document.getElementById('range');
     let tableBox = document.getElementById('tablebox');
     // let show = document.getElementById('showtable');
 
-    if (input <= '') {
+    if (input.value <= '') {
         Swal.fire({
             icon: "error",
             title: "Invaild",
             text: "Please Enter a Number",
         });
     } else {
-        for (let i = 1; i <= range; i++) {
-            tableBox.innerHTML = `<p id="showtable">${input} x ${i} = ${input * i}</p> `;
-
+        for (let i = 1; i <= range.value; i++) {
+            tableBox.innerHTML += `<p id="showtable">${input} x ${i} = ${input * i}</p> `;
         }
+        
     }
-    // window.location.reload();
+    
+    
 }
 
 
